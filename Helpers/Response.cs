c9cbd9f9af43a62f59c73863fe6c9b77a0b906e8
Rachid Amrani <rachid3alaoui@gmail.com>
2024-04-24 14:50:06 +0200

    fix Response class

--- Helpers/Response.cs
+++ Helpers/Response.cs
@@ -2,15 +2,8 @@
 {
     public class Response<T>
     {
-        public required T Data { get; set; }
+        public T? Data { get; set; }
         public required string Message { get; set; }
         public required bool IsSuccess { get; set; }
     }
-
-    public class Response
-    {
-        public required string Message { get; set; }
-        public required bool IsSuccess { get; set; }
-    }
-
 }
