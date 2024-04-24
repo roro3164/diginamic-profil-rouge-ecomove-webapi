25068211d2ab1ffd306ff6ea898f5c85f73e5f6a
Rachid Amrani <rachid3alaoui@gmail.com>
2024-04-24 09:17:47 +0200

    ajout des interfaces/repositories

---
+++ Repositories/VehicleStatus/IVehicleStatusRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecomove_back.Repositories.VehicleStatus
+{
+    public interface IVehicleStatusRepository
+    {
+        
+    }
+}
