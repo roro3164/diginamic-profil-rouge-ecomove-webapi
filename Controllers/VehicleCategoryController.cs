32654cf7d657dfbc131d979522b675f5be6b368d
Rachid Amrani <rachid3alaoui@gmail.com>
2024-04-24 14:53:42 +0200

    implémentation de la fonctionnalité de la création d'une catégorie

--- Controllers/VehicleCategoryController.cs
+++ Controllers/VehicleCategoryController.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ecomove_back.Data.Models;
+using ecomove_back.DTOs.VehicleCategoryDTOs;
+using ecomove_back.Helpers;
+using ecomove_back.Interfaces.IRepositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ecomove_back.Controllers
@@ -10,6 +10,26 @@ namespace ecomove_back.Controllers
     [Route("api/[controller]/[action]")]
     public class VehicleCategoryController : ControllerBase
     {
+        private readonly IVehicleCategoryRepository _vehicleCategoryRepository;
 
+        public VehicleCategoryController(IVehicleCategoryRepository vehicleCategoryRepository)
+        {
+            _vehicleCategoryRepository = vehicleCategoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicleCategory(VehicleCategoryForCreationDTO vehicleCategory)
+        {
+            Response<Category> response = await _vehicleCategoryRepository.CreateVehicleCategoryAsync(vehicleCategory);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            else
+            {
+                return Problem(response.Message);
+            }
+        }
     }
 }
---
+++ DTOs/VehicleCategoryDTOs/VehicleCategoryForCreationDTO.cs
@@ -0,0 +1,9 @@
+﻿using ecomove_back.Data.Models;
+
+namespace ecomove_back.DTOs.VehicleCategoryDTOs
+{
+    public class VehicleCategoryForCreationDTO
+    {
+        public string CategroyLabel { get; set; }
+    }
+}
