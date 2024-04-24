711ab3871f51bd8cd87239e578f39ce73be1a241
Rachid Amrani <rachid3alaoui@gmail.com>
2024-04-24 09:42:18 +0200

    ajout des controlleurs

---
+++ Controllers/VehicleCategoryController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecomove_back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class VehicleCategoryController : ControllerBase
+    {
+
+    }
+}
