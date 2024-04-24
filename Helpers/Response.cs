47883bd0f388593563edff1956381bc75c32cc3e
Rachid Amrani <rachid3alaoui@gmail.com>
2024-04-24 11:00:37 +0200

    ajout des helpers

---
+++ Helpers/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace ecomove_back.Helpers
+{
+    public class Response<T>
+    {
+        public required T Data { get; set; }
+        public required string Message { get; set; }
+        public required bool IsSuccess { get; set; }
+    }
+
+    public class Response
+    {
+        public required string Message { get; set; }
+        public required bool IsSuccess { get; set; }
+    }
+
+}
